e556ffe9c30096ae183e9dde688f70b289cc46a5
Manish Jha <manishkujha@gmail.com>
2017-01-18 11:45:10 -0500

    updated

---
+++ src/main/java/com/demo/TrackerappdemoApplication.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+
+
+@Configuration
+@ComponentScan(basePackages = {"com.demo", "com.demo.domain"})
+@EnableWs
+@SpringBootApplication
+public class TrackerappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerappdemoApplication.class, args);
+	}
+}
--- src/main/java/com/demo/WebServiceConfig.java
+++ src/main/java/com/demo/WebServiceConfig.java
@@ -1,5 +1,4 @@
-/*package com.demo;
-
+package com.demo;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
@@ -8,12 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 
+
 @Configuration
-public class WebServiceConfig extends WsConfigurerAdapter {
+public class WebServiceConfig {
+
+
 	@Bean
 	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
 		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
@@ -22,37 +23,21 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		return new ServletRegistrationBean(servlet, "/ws/*");
 	}
 
-	@Bean(name = "TrackerService")
-	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
-		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-		wsdl11Definition.setPortTypeName("TrackerServicePort");
-		wsdl11Definition.setLocationUri("/ws");
-		wsdl11Definition.setTargetNamespace("http://demo.com/ws/track/v12");
-		wsdl11Definition.setSchema(countriesSchema);
-		return wsdl11Definition;
-	}
+	 @Bean
+	    public Jaxb2Marshaller customerLocationClientMarshaller() {
+	        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+	        marshaller.setPackagesToScan("com.demo.fedex.domain", "com.demo.domain");
+	        return marshaller;
+	    }
+	    @Bean("TrackerService")
+	    public SimpleWsdl11Definition definition1509() {
+	        SimpleWsdl11Definition wsdl = new SimpleWsdl11Definition();
+	        wsdl.setWsdl(customerServiceWsdl());
+	        return wsdl;
+	    }
 
-	@Bean
-	public XsdSchema countriesSchema() {
-		return new SimpleXsdSchema(new ClassPathResource("tracker.xsd"));
-	}
-	
-    @Bean
-    public Jaxb2Marshaller customerLocationClientMarshaller() {
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setPackagesToScan("com.demo.fedex.domain", "com.demo.domain");
-        return marshaller;
-    }
-    @Bean(name = "TrackerService")
-    public SimpleWsdl11Definition definition1509() {
-        SimpleWsdl11Definition wsdl = new SimpleWsdl11Definition();
-        wsdl.setWsdl(customerServiceWsdl());
-        return wsdl;
-    }
-
-    @Bean
-    public Resource customerServiceWsdl() {
-        return new ClassPathResource("TrackingService.wsdl");
-    }
+	    @Bean
+	    public Resource customerServiceWsdl() {
+	        return new ClassPathResource("TrackingService.wsdl");
+	    }
 }
-*/
